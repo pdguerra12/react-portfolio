4d19811bef80d31a8f6c398e18b3987cc0d09587
pdguerra12 <paul.guerra09@gmail.com>
2022-04-13 23:55:01 -0500

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
 import "./App.css";
-import React, { useState } from "react";
-import Header from "./components/Header";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Resume from "./components/Resume";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-	const [page, setPage] = useState(1);
-
-	function updateState(pageNum) {
-		setPage(pageNum);
-	}
-
-	function renderPage() {
-		switch (page) {
-			case 0:
-				return <Header />;
-				break;
-			case 1:
-				return <About />;
-				break;
-			case 2:
-				return <Portfolio />;
-				break;
-			case 3:
-				return <Resume />;
-				break;
-			case 4:
-				return <Contact />;
-				break;
-			case 5:
-				return <Footer />;
-				break;
-		}
-	}
-
-	return (
-		<div className="App">
-			<Header updateState={updateState} />
-			<main>{renderPage()}</main>
-			<Footer />
-		</div>
-	);
+	return <Navbar />;
 }
 
 export default App;
